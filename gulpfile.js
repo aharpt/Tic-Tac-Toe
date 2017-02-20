b6dd06e81614385ee9d6bfb9f7fd6600ad9fd522
aaron.harpt <aaron.harpt@gmail.com>
2017-02-20 09:58:09 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp.src("./styles.scss")
+  .pipe(gulp.dest("./"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./styles.scss", ["sass"]);
+});
---
+++ scripts.js
@@ -0,0 +1,47 @@
+// jquery
+
+$(function() {
+});
+
+// jquery ui
+$(".fa").draggable({
+  start: function() {
+    $(this).addClass("highlight");
+  },
+  stop: function() {
+    $(this).removeClass("highlight");
+    $(this).draggable("disable").addClass("disabled");
+  },
+  containment: ".outer-space-container"
+
+});
+
+$(".space").droppable({
+  accept: ".fa"
+});
+
+// create random number function to decide who starts the game
+var randomNumber = function() {
+  return Math.floor(Math.random() * 2) + 1;
+}
+
+var randomNumberHolder = randomNumber();
+
+var numOfXsRemaining = 5;
+var numOfOsRemaining = 5;
+
+// decides who goes first, appends appropriate message, and changes appropriate elements html
+switch (randomNumberHolder) {
+  case 1:
+    document.querySelector(".decision-container").innerHTML = "<p>X's go first</p>";
+    $(".fa-opera").draggable("disable").addClass("disabled");
+    numOfXsRemaining -= 1;
+    break;
+
+  case 2:
+    document.querySelector(".decision-container").innerHTML = "<p>O's go first</p>";
+    $(".fa-times").draggable("disable").addClass("disabled");
+    numOfOsRemaining -= 1;
+    break;
+
+}
